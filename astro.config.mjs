0d13b00dae69e02cdb55a55542297ef70894f200
Flavien Bonvin <flavien.bonvin@pm.me>
2024-09-09 21:29:25 +0200

    feat: add web vitals

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,9 +7,11 @@ import icon from "astro-icon";
 
 import vercel from "@astrojs/vercel/serverless";
 
+import webVitals from "@astrojs/web-vitals";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), db(), icon()],
+  integrations: [tailwind(), db(), icon(), webVitals()],
   output: "server",
   adapter: vercel(),
 });
