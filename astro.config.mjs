a4418123f057ec678d60145c5b9275cb1a766d27
Flavien Bonvin <flavien.bonvin@pm.me>
2024-09-09 21:21:24 +0200

    🦆 Initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,15 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import db from "@astrojs/db";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), db(), icon()],
+  output: "server",
+  adapter: vercel(),
+});
